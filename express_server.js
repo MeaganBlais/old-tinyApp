3ac3fec442f330c19c89c6bb91d6224dd240936a
Meagan Blais <meaganblais@gmail.com>
2017-05-09 20:13:13 -0400

    intial commit

--- express_server.js
+++ express_server.js
@@ -2,10 +2,7 @@ var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 8080; // default port 8080
 
-var urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
-};
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
   res.end("Hello!");
